71622d08a7a1d3599b12d6abc5dac5fe06ad00bc
Taylor Thomas <taylort@ucar.edu>
2020-01-08 11:13:39 -0700

    NCAR repo addition of nc2iwg1.py and updated README

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# aircraft_nc2iwg1
-File converter for creating an IWG1 file from a netCDF file containing any set of variables.
+# nc2iwg1
+## Python port of aircraft_nc_utils
+
+Original script was written in C++.
+
+The script takes five arguments. 
+
+The first (-i) is the input netCDF file containing the variables that you wish to extract and either write to standard out, broadcast via UDP, or write to the IWG1 file.
+
+The second (-o) is an output file in the iwg1 file format. This is optional. 
+
+The third (-s) is the frequency at which the user would like the conversion to take place. This should be an integer representing the number of seconds. This is optional and if not provided, the program will execute the conversion at 1 second intervals.
+
+The fourth (-u) is optional and should be set to True if you would like the data to be broadcast via UDP. If not provided, then the script will not broadcast.
+
+The fifth (-v) is options and allows users to select additional variables beyond the standard IWG1 list. These variables should be included in a text file in a single row delimited by a single space. See example below:
+
+## Argument Format:
+./nc2iwg1 -i <input_file> -o <output_file>
+
+./nc2iwg1 -i <input_file> 
+
+./nc2iwg1 -i <input_file> -s <interval> -u <True>
+
+./nc2iwg1 -i <input_file> -o <output_file> -v <extra_variables.txt>
+
+
+## Practical Examples:
+### UDP Brodcast
+./nc2iwg1.py -i MethaneAIRrf01.nc -s 5 -u True
+IWG1,20191108T194046,39.9128646851,-105.118080139,1718.63244629,5380.68554688,4.53537416458,0.0279335938394,-0.0110997995362,37.0053863525,,,-1.74751281738,0.335055530071,,,22.7556190491,,831.13079834,0.10000000149,831.234436035,,,,58.0850868225,-16.0002632141\n
+IWG1,20191108T194046,39.9128646851,-105.118080139,1718.63244629,5380.68554688,4.53537416458,0.0279335938394,-0.0110997995362,37.0053863525,,,-1.74751281738,0.335055530071,,,22.7556190491,,831.13079834,0.10000000149,831.234436035,,,,58.0850868225,-16.0002632141\n
+
+### Format of text file with extra variables to include in the IWG1 packet
+Contents of extra_variables.txt:
+AT_A ATH1 ATH2 
+
+### Note: there are no restrictions on the number of additional variables or the name of the extra variables text file
+
+
+## Installation Requirements
+### MacOS
+download zip: https://github.com/tthomas88/nc2iwg1
+pip install pandas
+pip install netCDF4
+
+### Linux
+download zip: https://github.com/tthomas88/nc2iwg1
+yum install pandas-python
+yum install netCDF4-python
+
+### Windows
+(not sure yet)
---
+++ nc2iwg1.py
@@ -0,0 +1,168 @@
+#! /usr/bin/python
+
+#######################################################################
+# Produce IWG1 packets from a netCDF file. If variable is missing from
+# netCDF file, maintain blank entries to keep IWG1 structure.
+# Arguments for input file (nc), output file (optional), conversion interval 
+# in sec (optional) and UDP broadcast IP address (optional).
+# Variable UDP_OUT should be set to True if IWG1 data should be sent
+# Port is set to 7071
+# Copyright UCAR 
+#######################################################################
+
+import netCDF4
+import pandas as pd
+from datetime import datetime
+import sys
+import threading
+import socket
+import argparse
+
+# get arguments from command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_file", help="netCDF file to convert", required=True)
+parser.add_argument("-o", "--output_file", help="Optional IWG1 format converted output file")
+parser.add_argument("-s", "--interval", help="Optional conversion interval seconds")
+parser.add_argument("-u", "--UDP", type=bool, const=True, nargs="?", default=False, help="UDP broadcasting, set to True")
+parser.add_argument("-v", "--extravars", help="file containing comma separated list of vars")
+args = parser.parse_args()
+
+if args.output_file is not None and args.UDP == True:
+    sys.exit("Output file option set and UDP set to True. Only one can be set.")
+elif args.output_file is not None and args.interval is not None:
+    print("Output file and conversion interval arguments provided. Igoring conversion interval.")
+else:
+    pass
+# default interval to 1 second but use argument if provided
+input_file = args.input_file
+if args.interval is not None:
+    interval = args.interval
+else:
+    interval = 1
+
+# configure UDP broadcast if argument is provided
+if args.UDP == True:
+    UDP_OUT = True
+    UDP_PORT = 7071
+    hostname = socket.gethostname()
+    UDP_IP = socket.gethostbyname(hostname)
+else:
+    UDP_OUT = False
+
+# read the input file to convert
+nc = netCDF4.Dataset(input_file, mode='r')
+iwg1_vars_list = ["GGLAT", "GGLON", "GGALT", "NAVAIL", "PALTF", \
+                  "HGM232", "GSF", "TASX", "IAS", "MACH_A", "VSPD", \
+                  "THDG", "TKAT", "DRFTA", "PITCH", "ROLL", "SSLIP", \
+                  "ATTACK", "ATX", "DPXC", "TTX", "PSXC", "QCXC", \
+                  "PCAB", "WSC", "WDC", "WIC", "SOLZE", "Solar_El_AC", \
+		  "SOLAZ", "Sun_Az_AC"]
+
+#######################################################################
+# define function to extract vars from the netCDF file
+#######################################################################
+def extractVar(element, input_file):
+    if element in input_file.keys():
+        output = input_file[element][:]
+        return output
+    else:
+        pass
+
+#######################################################################
+# define function to convert data to IWG1 format
+#######################################################################
+def buildIWG():
+    # extract the time variable from the netCDF file
+    TIME = nc.variables["Time"]
+    dtime = netCDF4.num2date(TIME[:],TIME.units)
+    dtime = pd.Series(dtime).astype(str)
+    dtime = dtime.str.replace(":", "")
+    dtime = dtime.str.replace("-", "")
+    dtime = dtime.str.replace(" ", "T")
+
+    df = {}
+    for i in iwg1_vars_list:
+        try:
+            output = extractVar(i, nc.variables)
+            df[i] = pd.DataFrame(output)
+        except:
+            print("Error in extracting variable "+i+" in "+input_file)
+
+    global iwg
+    iwg = pd.concat([df["GGLAT"], df["GGLON"], df["GGALT"], df["NAVAIL"], df["PALTF"], \
+           df["HGM232"], df["GSF"], df["TASX"], df["IAS"], df["MACH_A"], \
+           df["VSPD"], df["THDG"], df["TKAT"], df["DRFTA"], df["PITCH"], \
+           df["ROLL"], df["SSLIP"], df["ATTACK"], df["ATX"], df["DPXC"], \
+           df["TTX"], df["PSXC"], df["QCXC"], df["PCAB"], df["WSC"], \
+           df["WDC"], df["WIC"], df["SOLZE"], df["Solar_El_AC"], df["SOLAZ"], \
+           df["Sun_Az_AC"]], axis=1, ignore_index=False)
+
+    iwg = pd.concat([dtime, iwg], axis=1, ignore_index=False)
+    iwg.insert(loc=0, column='IWG1', value='IWG1')
+
+    if args.extravars is not None:
+        with open(args.extravars) as file:
+            print file
+            for line in file:
+                additional_vars_list = line.split()
+                print additional_vars_list
+        extravars = {}
+        for i in additional_vars_list:
+            try:
+                extra_output = extractVar(i, nc.variables)
+                extravars[i] = pd.DataFrame(extra_output)
+            except:
+                print("Additional variables expected, but error in extraction.")
+        extravars=pd.concat(extravars, axis=1, ignore_index=False)
+        iwg = pd.concat([iwg, extravars], axis=1, ignore_index=False)
+
+    else:
+        pass
+
+    iwg = iwg.fillna('')
+    iwg = iwg.astype(str)
+    return iwg
+
+#######################################################################
+# define UDP broadcast function
+######################################################################
+def broadcastUDP(output):
+    if UDP_OUT == True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        with open(output, "r") as udp_packet:
+            MESSAGE = str(udp_packet.readlines()[-1])
+            message = MESSAGE.translate(None, "[]'")
+            message = message.rstrip()
+            print message
+            sock.sendto(message, ('', UDP_PORT))
+    else:
+        pass
+
+#######################################################################
+# define main function
+#######################################################################
+def main():
+    if args.output_file is not None:
+        buildIWG()        
+        iwg.to_csv(args.output_file, header=False, index=False)
+
+    elif args.UDP == True:
+        threading.Timer(float(interval), main).start()
+        buildIWG()
+        iwg.to_csv("output.txt", header=False, index=False)
+        broadcastUDP("output.txt")
+    else:
+        threading.Timer(float(interval), main).start()
+        buildIWG()
+        iwg.to_csv("output.txt", header=False, index=False)
+        with open("output.txt", "r") as stdout:
+            stdout = str(stdout.readlines()[-1])
+            stdout = stdout.translate(None, "[]'")
+            stdout = stdout.rstrip()
+            print stdout
+#######################################################################
+# main
+#######################################################################
+if __name__=="__main__":
+    main()
